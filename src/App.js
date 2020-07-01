c63dee8bab2c325574f04e6c0391aaabe20f0645
yudahadi14 <yudahadi14@gmail.com>
2020-07-01 13:58:43 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://103.78.27.37/E_Layanan/index.php/VideoUpload"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lanjutkan
         </a>
       </header>
     </div>
